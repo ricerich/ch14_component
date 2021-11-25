b0ab9e16d1591fa4fef800ccca1c5baac4997da5
ihongss <36065549+ihongss@users.noreply.github.com>
2021-11-25 11:50:50 +0900

    14장 파일분리 2번

--- ch14_2_broadcast_receiver/src/main/java/com/example/ch14_2_broadcast_receiver/MainActivity.java
+++ ch14_2_broadcast_receiver/src/main/java/com/example/ch14_2_broadcast_receiver/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     ImageView ivBattery;
     EditText edtBattery;
 
+    MyBroadcastReceiver br;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,52 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    MyBroadcastReceiver br = new MyBroadcastReceiver(ivBattery, edtBattery);
-//    MyBroadcastReceiver br = new MyBroadcastReceiver(MainActivity.this);
-
-//    BroadcastReceiver br1 = new BroadcastReceiver() {
-//        @Override
-//        public void onReceive(Context context, Intent intent) {
-//
-//        }
-//    };
-
-    BroadcastReceiver br = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-
-            if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
-                int remain = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-                edtBattery.setText("현재 충전량 : " + remain + "\n");
-
-                if (remain >= 90)
-                    ivBattery.setImageResource(R.drawable.battery_100);
-                else if (remain >= 70)
-                    ivBattery.setImageResource(R.drawable.battery_80);
-                else if (remain >= 50)
-                    ivBattery.setImageResource(R.drawable.battery_60);
-                else if (remain >= 10)
-                    ivBattery.setImageResource(R.drawable.battery_20);
-                else
-                    ivBattery.setImageResource(R.drawable.battery_0);
-
-                int plug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0);
-                switch (plug) {
-                    case 0:
-                        edtBattery.append("전원 연결 : 안됨");
-                        break;
-                    case BatteryManager.BATTERY_PLUGGED_AC:
-                        edtBattery.append("전원 연결 : AC어댑터 연결됨");
-                        break;
-                    case BatteryManager.BATTERY_PLUGGED_USB:
-                        edtBattery.append("전원 연결 : USB 연결됨");
-                        break;
-                }
-            }
-        }
-    };
-
     @Override
     protected void onPause() {
         super.onPause();
@@ -82,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        br = new MyBroadcastReceiver(ivBattery, edtBattery);
+
         IntentFilter iFilter = new IntentFilter();
         iFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
         registerReceiver(br, iFilter);
